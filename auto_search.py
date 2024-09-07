8301974b8aa1d2c48346f223f982b4dc50ceb2b8
palmergp <gp.palmer@verizon.net>
2024-09-07 14:24:02 -0400

    Modified to use random words for search

--- auto_search.py
+++ auto_search.py
@@ -2,6 +2,7 @@ import autopy
 import random
 import time
 import pickle
+from wonderwords import RandomWord
 from configure_auto_search import configure
 
 SEARCHES = 50
@@ -42,26 +43,32 @@ def main():
 		autopy.key.type_string(random_search(), wpm=2000)
 		autopy.key.tap(autopy.key.Code.RETURN)
 
-		time.sleep(20)
+		time.sleep(random.randint(20,65))
 
 	print("Searches complete. Minimizing Browser")
 	click(values["icon_x"], values["icon_y"])
 	
 
 def random_search():
-    # List of random words to search
-    words = ["movies", "best", "worst", "and", "actors", "games", "top 10",
-             "of 2000s", "shows","tv shows","songs","singers","artists",
-             "shaimus", "recipes", "food", "2010's", "1990's", "funny",
-             "cats","dogs","hamsters","video games"]
+	# List of random words to search
+	words = ["movies", "best", "worst", "and", "actors", "games", "top 10",
+			 "of 2000s", "shows","tv shows","songs","singers","artists",
+			 "shaimus", "recipes", "food", "2010's", "1990's", "funny",
+			 "cats","dogs","hamsters","video games","disasters","weather"]
+	if random.randint(0, 100) > 95:
+		# Do a search for a random zip code weather
+		zipcode = random.randint(501, 99950)
+		ran_search = f"weather in {zipcode}"
 
-    search_count = random.randint(1,5)
-    ran_search = ""
-    for i in range(search_count):
-        ran_word = words[random.randint(1,len(words)-1)]
-        ran_search = ran_search + " " + ran_word
+	else:
+		search_count = random.randint(1,5)
+		ran_search = ""
+		for i in range(search_count):
+			ran_word = RandomWord().word()
+			ran_search = ran_search + " " + ran_word
 
-    return ran_search
+	print(f"Searching: {ran_search}")
+	return ran_search
 
 def click(x,y):
 	autopy.mouse.move(x,y)
@@ -69,4 +76,4 @@ def click(x,y):
 
 
 if __name__ == "__main__":
-    main()
+	main()
